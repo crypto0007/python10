54accdf46e20d217832da6a00279a775d95336d5
crypto0007 <50778638+crypto0007@users.noreply.github.com>
2019-09-27 15:18:40 +0530

    Add files via upload

---
+++ cvsv.py
@@ -0,0 +1,21 @@
+class person:
+    ocuppation="dorctor"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def prin(self):
+        print(self.name)
+        print(self.age)
+
+
+s0=person("ishaan",25)
+#s1=person("is",5)
+
+print(s0.name)
+print(s0.age)
+print(s0.ocuppation)
+
+#print(s1.name)
+#print(s1.age)
+#print(s1.ocuppation)
